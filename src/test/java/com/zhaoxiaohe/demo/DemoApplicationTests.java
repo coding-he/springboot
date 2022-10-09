8167112fe1708f28a8ab2512f1f179d4fb2f0ed3
ZhaoXiaoHe <you@example.com>
2022-10-09 15:12:27 +0800

    init

---
+++ src/test/java/com/zhaoxiaohe/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoxiaohe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
